a1adc6f0528d5e6931e6a2887a89decaf34b0077
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-03-12 15:22:09 +0000

    usando variaveis

---
+++ script.js
@@ -0,0 +1,16 @@
+// var 
+//var altura =5;
+//var conprimento =7;
+
+//area = altura * conprimento;
+//console.log(area);
+var area;
+
+let forma ="retangulo";
+altura = 5
+let conprimento =7;
+
+if (forma=== retangulo"){
+area = altura * conprimento;
+}else {
+console.log(area);
