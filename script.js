80204bacd031a71d41ea9162f28569c4761b0864
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-03-19 14:58:51 +0000

    .

--- script.js
+++ script.js
@@ -4,17 +4,31 @@
 
 //area = altura * conprimento;
 //console.log(area);
-var area;
+//var area;
 
-let forma ="retangulo";
-altura = 5
-let conprimento =7;
-let area;
+//let forma ="retangulo";
+//altura = 5
+//let conprimento =7;
+//let area;
 
 //if (forma == retangulo ){
 //area = altura * conprimento;
 //}else {
-     area = (altura * conprimento) / 2;
+     //area = (altura * conprimento) / 2;
 
 //}
 //console.log(area);
+//console.log(area)
+
+const forma = 'triangulo';
+
+const altura = 5;
+const conprimento = 7;
+let area;
+if (forma === 'quadrado'){
+   area = altura * conprimento;
+}else{
+     area = (altura * conprimento)/2;
+}    
+ console.log (area);
+ 
